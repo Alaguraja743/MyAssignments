fa261a9f0d9c39925dbd5ad8fb02832359e45bb6
Alaguraja743 <alagu.raja743@gmail.com>
2025-11-23 19:28:26 +0530

    Add files via upload

---
+++ week2.day1/LibraryManagement.java
@@ -0,0 +1,13 @@
+	package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library library = new Library();
+		
+		library.addBook("Java");
+		library.issueBook();
+
+	}
+
+}
