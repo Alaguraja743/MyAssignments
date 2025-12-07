7c08ecba81e398d6dea61aac54b00a64f9399b4e
Alaguraja743 <alagu.raja743@gmail.com>
2025-12-07 19:28:33 +0530

    Delete week5.day1 directory

--- week5.day1/Abstraction/MySqlConnection.java
+++
@@ -1,7 +0,0 @@
-package week4.day1;
-
-public abstract class MySqlConnection implements DatabaseConnection {
-	
-	public abstract  void executeQuery();
-
-}
--- week5.day1/Abtractioninterface/CanaraBank.java
+++
@@ -1,7 +0,0 @@
-package week4.day1;
-
-public abstract class CanaraBank implements payments {
-
-    public abstract void recordPaymentDetails();
-
-}
